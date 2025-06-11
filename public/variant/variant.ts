3ea277f85ca5c75d96b6ea1c0fe3e3094210f242
Akshet Patel <akshetp.ap@gmail.com>
2025-06-11 10:38:04 +0100

    Enhance fadeIn function to support left and right directions

--- public/variant/variant.ts
+++ public/variant/variant.ts
@@ -1,16 +1,23 @@
 // animations/fadeIn.ts
-export const fadeIn = (direction: 'up' | 'down', delay: number) => {
+export const fadeIn = (
+  direction: 'up' | 'down' | 'left' | 'right',
+  delay: number
+) => {
   const isUp = direction === 'up';
   const isDown = direction === 'down';
+  const isLeft = direction === 'left';
+  const isRight = direction === 'right';
 
   return {
     hidden: {
       y: isUp ? 40 : isDown ? -40 : 0,
+      x: isLeft ? -40 : isRight ? 40 : 0,
       scale: 0.9,
       opacity: 0,
     },
     show: {
       y: 0,
+      x: 0,
       scale: 1,
       opacity: 1,
       transition: {
@@ -22,4 +29,3 @@ export const fadeIn = (direction: 'up' | 'down', delay: number) => {
     },
   };
 };
-
